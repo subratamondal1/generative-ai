61bbbfa089399dc97c1f2d64f9893b855fe17784
Subrata Mondal <subratasubha2@gmail.com>
2024-07-30 15:11:05 +0530

    Completed SimpleLLMs

--- src/apis.py
+++ src/apis.py
@@ -7,3 +7,4 @@ load_dotenv(dotenv_path=find_dotenv(), override=True)
 OPENAI_API_KEY:str|None=os.getenv(key="OPENAI_API_KEY")
 PINECONE_API_KEY:str|None=os.getenv(key="PINECONE_API_KEY")
 LANGCHAIN_API_KEY:str|None=os.getenv(key="LANGCHAIN_API_KEY")
+
---
+++ src/simple_llm.py
@@ -0,0 +1,59 @@
+from langchain.llms.openai import OpenAI
+
+from apis import OPENAI_API_KEY
+
+from langchain.llms.openai import OpenAI
+
+llm=OpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    temperature=0.9, 
+    max_tokens=512
+  )
+print("\nllm:",llm)
+
+prompt="Explain LLM Wrappers and it's use-cases in three sentences."
+output=llm(prompt)
+print("\noutput:",output)
+
+prompt_token_length=llm.get_num_tokens(prompt)
+print(f"Prmopt token length: {prompt_token_length}")
+
+### generate multiple prompts
+print("\ngenerate multiple prompts\n")
+
+prompt1="... is the city of joy."
+prompt2="Top 10 richest persons are ..."
+output=llm.generate([prompt1, prompt2])
+
+print(output)
+print(len(output.generations), output.generations)
+print(output.generations[0][0])
+# access only the first prompt result
+print(output.generations[0][0].text)
+
+### Generate Multiple Responses for Single Prompt
+print("\nGenerate Multiple Responses from Single Prompt\n")
+
+prompt="Write tagline for a software company that does outsourcing."
+outputs=llm.generate([prompt], n=2)
+
+print(outputs)
+print(len(outputs.generations[0]), outputs.generations)
+
+for index,output in enumerate(outputs.generations[0]):
+  print(output.text)
+
+### Generate Multiple Responses from Multiple Prompts
+print("\nGenerate Multiple Responses from Multiple Prompts\n")
+
+# asked llm to generate multiple responses
+prompt1="Write tagline for an AI Company."
+prompt2="Write a tagline for a Sweet Company."
+outputs=llm.generate([prompt1,prompt2] * 4)
+
+print(outputs)
+print(len(outputs.generations[0]), outputs.generations)
+print(outputs.generations[0])
+
+for index,output in enumerate(outputs.generations):
+  print(output[0].text)
