4700db2c0a625ff2c3e0127c7db22b8e040c65fa
Subrata Mondal <subratasubha2@gmail.com>
2024-08-05 11:47:18 +0530

    Langchain Chaining with the Legacy Version: LLMChain class

--- ai_code_generator/main.py
+++ ai_code_generator/main.py
@@ -1,4 +1,6 @@
 from langchain_openai import ChatOpenAI
+from langchain.chains.llm import LLMChain
+from langchain_core.prompts import PromptTemplate
 
 from src.apis import OPENAI_API_KEY
 
@@ -7,9 +9,23 @@ llm = ChatOpenAI(
     api_key = OPENAI_API_KEY,
     model = "gpt-3.5-turbo-0125"
 )
+
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that would {task}.",
+    input_variables = ["language", "task"]
+)
+chain = LLMChain(
+    prompt = code_prompt,
+    llm = llm
+)
 breakpoint()
 
-result = llm.invoke(input="Write me a Poem")
+result = chain.invoke(
+    input = {
+        "language":"Python",
+        "task":"print the fibbonacci sequence"
+    }
+)
 breakpoint()
 
 print(result)
